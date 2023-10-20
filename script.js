924df9d4d4874f429b795a8f6c4d6bb27c98fb69
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-20 22:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
  document.getElementById("enterBtn").addEventListener("click", function() {
+            // Change the text in the p tag to "Entered Metaverse"
             document.getElementById("status").textContent = "Entered Metaverse";
+            
+            // Display the same text in the h1 tag
             document.getElementById("output").textContent = "Entered Metaverse";
         });
