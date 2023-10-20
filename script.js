1fcf70af02de1ddefb08725202690548c5acc419
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-20 21:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-  let status = document.getElementById("status");
-        let enterBtn = document.getElementById("enterBtn");
-        let output = document.getElementById("output");
-
-        // Add a click event listener to the button
-        enterBtn.addEventListener("click", function() {
-            // Change the text in the p tag
-            status.textContent = "Entered Metaverse";
-
-            // Display the updated text in the h1 tag
-            output.textContent = "Entered Metaverse";
+ document.getElementById("enterBtn").addEventListener("click", function() {
+            document.getElementById("status").textContent = "Entered Metaverse";
+            document.getElementById("output").textContent = "Entered Metaverse";
         });
